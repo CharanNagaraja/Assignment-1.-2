df5d88ee8a93605ca912e81790dc8233e5622d1e
CN045696 <CN045696@W1571417.northamerica.cerner.net>
2017-08-02 22:10:50 +0530

    Write a program to print the result of the following expressions provided the integer variable a is 20 and b is 10.

---
+++ src/integer/integer.java
@@ -0,0 +1,19 @@
+package integer;
+
+public class integer {
+	public static void main(String[] args) {
+		
+	    int a=20;
+		int b= a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+		
+		System.out.println("The result:"+a);
+		System.out.println("The result:"+b);
+		System.out.println("The result:"+c);
+		System.out.println("The result:"+d);
+		System.out.println("The result:"+e);
+	}
+
+}
